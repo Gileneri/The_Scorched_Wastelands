07bcebf8758190ad686be24018ec9a0dde891d08
Gileneri <Usuario@DESKTOP-CKS0B8C>
2023-04-25 15:56:05 -0300

    0.25 update (added a few more blocks and custom objects, also updateded existing textures for the resourcepack update)

--- src/main/java/net/mcreator/thescorchedwastelands/block/DrySandBlock.java
+++ src/main/java/net/mcreator/thescorchedwastelands/block/DrySandBlock.java
@@ -2,31 +2,12 @@
 package net.mcreator.thescorchedwastelands.block;
 
 import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.common.ToolType;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.IPlantable;
 
-import net.minecraft.world.gen.feature.template.RuleTest;
-import net.minecraft.world.gen.feature.template.IRuleTestType;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.feature.OreFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.World;
-import net.minecraft.world.ISeedReader;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.util.registry.WorldGenRegistries;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.Rotation;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.RegistryKey;
 import net.minecraft.util.Direction;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.StateContainer;
@@ -39,14 +20,12 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
 import net.mcreator.thescorchedwastelands.itemgroup.ScorchedWastelandsItemGroup;
 import net.mcreator.thescorchedwastelands.TheScorchedWastelandsModElements;
 
-import java.util.Random;
 import java.util.List;
 import java.util.Collections;
 
@@ -57,8 +36,6 @@ public class DrySandBlock extends TheScorchedWastelandsModElements.ModElement {
 
 	public DrySandBlock(TheScorchedWastelandsModElements instance) {
 		super(instance, 12);
-		MinecraftForge.EVENT_BUS.register(this);
-		FMLJavaModLoadingContext.get().getModEventBus().register(new FeatureRegisterHandler());
 	}
 
 	@Override
@@ -122,229 +99,5 @@ public class DrySandBlock extends TheScorchedWastelandsModElements.ModElement {
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-	}
-
-	private static Feature<OreFeatureConfig> feature = null;
-	private static ConfiguredFeature<?, ?> configuredFeature = null;
-	private static IRuleTestType<CustomRuleTest> CUSTOM_MATCH = null;
-
-	private static class CustomRuleTest extends RuleTest {
-		static final CustomRuleTest INSTANCE = new CustomRuleTest();
-		static final com.mojang.serialization.Codec<CustomRuleTest> codec = com.mojang.serialization.Codec.unit(() -> INSTANCE);
-
-		public boolean test(BlockState blockAt, Random random) {
-			boolean blockCriteria = false;
-			if (blockAt.getBlock() == Blocks.STONE)
-				blockCriteria = true;
-			if (blockAt.getBlock() == Blocks.ORANGE_TERRACOTTA)
-				blockCriteria = true;
-			if (blockAt.getBlock() == Blocks.RED_SAND)
-				blockCriteria = true;
-			if (blockAt.getBlock() == Blocks.SAND)
-				blockCriteria = true;
-			if (blockAt.getBlock() == Blocks.DIRT)
-				blockCriteria = true;
-			return blockCriteria;
-		}
-
-		protected IRuleTestType<?> getType() {
-			return CUSTOM_MATCH;
-		}
-	}
-
-	private static class FeatureRegisterHandler {
-		@SubscribeEvent
-		public void registerFeature(RegistryEvent.Register<Feature<?>> event) {
-			CUSTOM_MATCH = Registry.register(Registry.RULE_TEST, new ResourceLocation("the_scorched_wastelands:dry_sand_match"),
-					() -> CustomRuleTest.codec);
-			feature = new OreFeature(OreFeatureConfig.CODEC) {
-				@Override
-				public boolean generate(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos, OreFeatureConfig config) {
-					RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
-					boolean dimensionCriteria = false;
-					if (dimensionType == World.OVERWORLD)
-						dimensionCriteria = true;
-					if (!dimensionCriteria)
-						return false;
-					return super.generate(world, generator, rand, pos, config);
-				}
-			};
-			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 16)).range(128)
-					.square().func_242731_b(10);
-			event.getRegistry().register(feature.setRegistryName("dry_sand"));
-			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("the_scorched_wastelands:dry_sand"), configuredFeature);
-		}
-	}
-
-	@SubscribeEvent
-	public void addFeatureToBiomes(BiomeLoadingEvent event) {
-		boolean biomeCriteria = false;
-		if (new ResourceLocation("badlands").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("badlands_plateau").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("bamboo_jungle").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("bamboo_jungle_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("basalt_deltas").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("beach").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("birch_forest").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("birch_forest_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("cold_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("crimson_forest").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("dark_forest").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("deep_cold_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("deep_frozen_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("dark_forest_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("deep_lukewarm_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("deep_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("deep_warm_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("desert").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("desert_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("desert_lakes").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("end_barrens").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("end_highlands").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("end_midlands").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("eroded_badlands").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("flower_forest").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("forest").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("wooded_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("frozen_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("plains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("frozen_river").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("plains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("ice_spikes").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("jungle").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("jungle_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("lukewarm_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("modified_badlands_plateau").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("modified_gravelly_mountains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("modified_jungle").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("modified_jungle_edge").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("modified_wooded_badlands_plateau").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("mountain_edge").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("plains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("snowy_beach").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("snowy_mountains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("snowy_tundra").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("snowy_taiga").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("snowy_taiga_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("snowy_taiga_mountains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("mushroom_field_shore").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("nether_wastes").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("tall_birch_forest").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("tall_birch_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("giant_tree_taiga").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("giant_tree_taiga_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("giant_spruce_taiga").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("giant_spruce_taiga_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("plains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("river").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("savanna").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("savanna_plateau").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("shattered_savanna_plateau").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("small_end_islands").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("soul_sand_valley").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("jungle_edge").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("plains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("stone_shore").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("sunflower_plains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("swamp").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("swamp_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("taiga").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("taiga_hills").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("taiga_mountains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("the_end").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("the_void").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("warm_ocean").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("warped_forest").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("wooded_mountains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("gravelly_mountains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("mountains").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("shattered_savanna").equals(event.getName()))
-			biomeCriteria = true;
-		if (new ResourceLocation("wooded_badlands_plateau").equals(event.getName()))
-			biomeCriteria = true;
-		if (!biomeCriteria)
-			return;
-		event.getGeneration().getFeatures(GenerationStage.Decoration.UNDERGROUND_ORES).add(() -> configuredFeature);
 	}
 }
--- src/main/java/net/mcreator/thescorchedwastelands/block/MetaliccolumnBlock.java
+++ src/main/java/net/mcreator/thescorchedwastelands/block/MetaliccolumnBlock.java
@@ -2,7 +2,10 @@
 package net.mcreator.thescorchedwastelands.block;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.IBlockReader;
 import net.minecraft.util.math.BlockPos;
@@ -10,6 +13,8 @@ import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
@@ -37,16 +42,27 @@ public class MetaliccolumnBlock extends TheScorchedWastelandsModElements.ModElem
 				() -> new BlockItem(block, new Item.Properties().group(ScorchedWastelandsItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
 
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
 			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 3f).setLightLevel(s -> 0).harvestLevel(2)
-					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+					.harvestTool(ToolType.PICKAXE).setRequiresTool().notSolid().setOpaque((bs, br, bp) -> false));
 			setRegistryName("metaliccolumn");
 		}
 
 		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
 		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
-			return 15;
+			return 0;
 		}
 
 		@Override
